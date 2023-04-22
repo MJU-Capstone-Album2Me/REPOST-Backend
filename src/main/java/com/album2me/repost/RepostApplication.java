c7ddeadc0e0ba0ad4a10d0125643e06529ce0966
JEONG-JIWHAN <jjw981210@gmail.com>
2023-04-22 19:30:22 +0900

    chore: project settings

---
+++ src/main/java/com/album2me/repost/RepostApplication.java
@@ -0,0 +1,13 @@
+package com.album2me.repost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/album2me/repost/RepostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.album2me.repost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
