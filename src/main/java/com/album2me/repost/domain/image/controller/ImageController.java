408a82108a826f0eafe0262b33059b37b5a183f4
JEONG-JIWHAN <jjw981210@gmail.com>
2023-05-05 02:49:40 +0900

    feat: 이미지 업로드 기능 구현

---
+++ src/main/java/com/album2me/repost/domain/image/controller/ImageController.java
@@ -0,0 +1,27 @@
+package com.album2me.repost.domain.image.controller;
+
+import com.album2me.repost.domain.image.dto.UploadImageRequest;
+import com.album2me.repost.domain.image.dto.UploadImageResponse;
+import com.album2me.repost.domain.image.service.ImageService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/image")
+@RequiredArgsConstructor
+public class ImageController {
+    private final ImageService imageService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public UploadImageResponse uploadImage(@ModelAttribute @Valid UploadImageRequest uploadImageRequest) {
+        UploadImageResponse uploadImageResponse = imageService.uploadImage(uploadImageRequest);
+        return uploadImageResponse;
+    }
+}
---
+++ src/main/java/com/album2me/repost/domain/image/dto/UploadImageRequest.java
@@ -0,0 +1,10 @@
+package com.album2me.repost.domain.image.dto;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.web.multipart.MultipartFile;
+
+public record UploadImageRequest(
+        @NotNull(message = "파일이 존재하지 않습니다.")
+        MultipartFile image
+) {
+}
