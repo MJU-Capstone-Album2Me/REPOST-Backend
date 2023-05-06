cbbd05bb19724f4a16b638564e6f08b33ddd5164
stophwan <jjw981210@gmail.com>
2023-05-07 07:41:18 +0900

    feat: User 도메인 권한 추가

--- src/main/java/com/album2me/repost/domain/user/model/User.java
+++ src/main/java/com/album2me/repost/domain/user/model/User.java
@@ -1,11 +1,7 @@
 package com.album2me.repost.domain.user.model;
 
 import com.album2me.repost.global.common.BaseTimeColumn;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,13 +23,15 @@ public class User extends BaseTimeColumn {
 
     @Column(length = 20, unique = true, nullable = false)
     private String nickName;
-    private String role;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.ROLE_USER;
 
     @Builder
     protected User(final String authId, final String password, final String nickName) {
         this.authId = authId;
         this.password = password;
         this.nickName = nickName;
-        this.role = "ROLE_USER";
     }
+
 }
