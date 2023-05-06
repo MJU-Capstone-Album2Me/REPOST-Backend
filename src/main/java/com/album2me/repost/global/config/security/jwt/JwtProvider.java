127ab6d2a4d79e85d4199a2e036aae4e7a98cf37
stophwan <jjw981210@gmail.com>
2023-05-07 07:42:48 +0900

    feat: 토큰 검증 기능 구현

--- src/main/java/com/album2me/repost/global/config/security/jwt/JwtProvider.java
+++ src/main/java/com/album2me/repost/global/config/security/jwt/JwtProvider.java
@@ -1,5 +1,6 @@
 package com.album2me.repost.global.config.security.jwt;
 
+import com.album2me.repost.domain.user.model.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.JWTVerifier;
@@ -8,6 +9,8 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.interfaces.Claim;
 import java.util.Collection;
 import java.util.Date;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
@@ -21,13 +24,47 @@ public class JwtProvider {
         this.algorithm = Algorithm.HMAC256(jwtProperties.getClientSecret());
     }
 
-    public String createAccessToken(String authId, String role){
-        String accessToken = JWT.create()
+    public String createAccessToken(User user){
+        Date now = new Date();
+        return JWT.create()
                 .withIssuer(jwtProperties.getIssuer())
-                .withClaim("userKey", authId)
-                .withClaim("role", role)
+                .withClaim("id", user.getId())
+                .withClaim("authId", user.getAuthId())
+                .withArrayClaim("roles", new String[]{user.getRole().name()})
+                .withIssuedAt(now)
+                .withExpiresAt(new Date(now.getTime() + jwtProperties.getExpirySeconds()))
                 .sign(algorithm);
-        return accessToken;
+    }
+
+    public Claims verifyToken(String token) {
+        JWTVerifier jwtverifier = JWT.require(algorithm).withIssuer(jwtProperties.getIssuer()).build();
+        return new Claims(jwtverifier.verify(token));
+    }
+
+    static public class Claims {
+        Long id;
+        String authId;
+        String[] roles;
+        Date iat;
+        Date exp;
+
+
+        Claims(DecodedJWT decodedJWT) {
+            Claim id = decodedJWT.getClaim("id");
+            if(!id.isNull()){
+                this.id = id.asLong();
+            }
+            Claim authId = decodedJWT.getClaim("authId");
+            if(!authId.isNull()){
+                this.authId = authId.asString();
+            }
+            Claim roles = decodedJWT.getClaim("roles");
+            if(!roles.isNull()){
+                this.roles = roles.asArray(String.class);
+            }
+            this.iat = decodedJWT.getIssuedAt();
+            this.exp = decodedJWT.getExpiresAt();
+        }
     }
 
 }
