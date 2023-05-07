8cd5bf80d9c122a52ca3b8f12622f3f03ac2ac26
wonjunYou <youwjune@gmail.com>
2023-05-07 11:41:43 +0900

    refactor : create, update DTO 분리

--- src/main/java/com/album2me/repost/domain/album/dto/request/AlbumUpdateRequest.java
+++ src/main/java/com/album2me/repost/domain/album/dto/request/AlbumUpdateRequest.java
@@ -1,14 +1,17 @@
 package com.album2me.repost.domain.album.dto.request;
 
 import com.album2me.repost.domain.album.model.Album;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class AlbumRequest {
+public class AlbumUpdateRequest {
 
+    @NotNull(message = "앨범명이 입력되지 않았습니다.")
     private String name;
+
     private String thumbnailUrl;
 
     @Builder
