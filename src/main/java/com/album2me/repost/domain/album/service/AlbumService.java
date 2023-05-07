c1c901a1452d24c26274212b6c1e028e7007c92a
wonjunYou <youwjune@gmail.com>
2023-05-07 11:46:37 +0900

    feat : 매핑된 연관관계 정보를 함께 저장하기 위해 Service 로직 추가

--- src/main/java/com/album2me/repost/domain/album/service/AlbumService.java
+++ src/main/java/com/album2me/repost/domain/album/service/AlbumService.java
@@ -1,9 +1,14 @@
 package com.album2me.repost.domain.album.service;
 
-import com.album2me.repost.domain.album.dto.request.AlbumRequest;
+import com.album2me.repost.domain.album.dto.request.AlbumCreateRequest;
+import com.album2me.repost.domain.album.dto.request.AlbumUpdateRequest;
 import com.album2me.repost.domain.album.dto.response.AlbumResponse;
 import com.album2me.repost.domain.album.model.Album;
 import com.album2me.repost.domain.album.repository.AlbumRepository;
+import com.album2me.repost.domain.room.model.Room;
+import com.album2me.repost.domain.room.repository.RoomRepository;
+import com.album2me.repost.domain.user.model.User;
+import com.album2me.repost.domain.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,6 +24,8 @@ import java.util.stream.Collectors;
 public class AlbumService {
 
     private final AlbumRepository albumRepository;
+    private final UserRepository userRepository;
+    private final RoomRepository roomRepository;
 
     public List<AlbumResponse> showAll() {
         final List<Album> albums = albumRepository.findAll();
@@ -28,10 +36,20 @@ public class AlbumService {
     }
 
     @Transactional
-    public AlbumResponse create(final AlbumRequest albumRequest) {
-        final Album newAlbum = albumRepository.save(albumRequest.toEntity());
+    public Long create(final Long userId, final AlbumCreateRequest albumCreateRequest) {
+        final User user = findUserById(userId);
+        final Room room = findRoomById(albumCreateRequest.getRoomId());
 
-        return AlbumResponse.from(newAlbum);
+        final Long albumId = createAlbum(user, room, albumCreateRequest);
+
+        return albumId;
+    }
+
+    private Long createAlbum(final User user, final Room room, final AlbumCreateRequest albumCreateRequest) {
+        final Album newAlbum = albumCreateRequest.toEntity(user, room);
+
+        return albumRepository.save(newAlbum)
+                .getId();
     }
 
     @Transactional
@@ -48,5 +66,20 @@ public class AlbumService {
                 .orElseThrow(IllegalArgumentException::new);
 
         albumRepository.delete(album);
+    }
+
+    private User findUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NoSuchElementException("해당 id로 User를 찾을 수 없습니다."));
+    }
+
+    private Room findRoomById(Long roomId) {
+        return roomRepository.findById(roomId)
+                .orElseThrow(() -> new NoSuchElementException("해당 id로 Room을 찾을 수 없습니다."));
+    }
+
+    private Album findAlbumById(Long albumId) {
+        return albumRepository.findById(albumId)
+                .orElseThrow(() -> new NoSuchElementException("해당 id로 Album을 찾을 수 없습니다."));
     }
 }
