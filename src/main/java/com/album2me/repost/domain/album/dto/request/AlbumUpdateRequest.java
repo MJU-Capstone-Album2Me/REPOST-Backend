3d3d08913c402c4a78ebd4bd66b841736bb21ff7
wonjunYou <youwjune@gmail.com>
2023-05-09 03:43:18 +0900

    refactor : Request DTO에 대해 lombok대신 record 사용하도록 변경

--- src/main/java/com/album2me/repost/domain/album/dto/request/AlbumUpdateRequest.java
+++ src/main/java/com/album2me/repost/domain/album/dto/request/AlbumUpdateRequest.java
@@ -2,19 +2,13 @@ package com.album2me.repost.domain.album.dto.request;
 
 import com.album2me.repost.domain.album.model.Album;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-public class AlbumUpdateRequest {
+public record AlbumUpdateRequest (
+    @NotNull(message = "앨범명이 입력되지 않았습니다.") String name,
+    String thumbnailUrl
 
-    @NotNull(message = "앨범명이 입력되지 않았습니다.")
-    private String name;
+) {
 
-    private String thumbnailUrl;
-
-    @Builder
     public Album toEntity() {
         return Album.builder()
                 .name(name)
--- src/main/java/com/album2me/repost/domain/post/dto/request/PostUpdateRequest.java
+++ src/main/java/com/album2me/repost/domain/post/dto/request/PostUpdateRequest.java
@@ -3,19 +3,13 @@ package com.album2me.repost.domain.post.dto.request;
 import com.album2me.repost.domain.post.model.Post;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-public class PostUpdateRequest {
-    @NotNull(message = "제목이 없습니다.")
-    private String title;
+public record PostUpdateRequest (
+    @NotNull(message = "제목이 없습니다.") String title,
 
-    @NotNull(message = "내용이 없습니다.")
-    private String contents;
+    @NotNull(message = "내용이 없습니다.") String contents
+) {
 
-    @Builder
     public Post toEntity() {
         return Post.builder()
                 .title(title)
