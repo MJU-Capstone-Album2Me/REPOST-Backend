199973d01afb61663711cff3facd3022d924dbb0
wonjunYou <youwjune@gmail.com>
2023-05-09 03:59:14 +0900

    refactor : Response DTO record로 변경

--- src/main/java/com/album2me/repost/domain/post/dto/response/PostResponse.java
+++ src/main/java/com/album2me/repost/domain/post/dto/response/PostResponse.java
@@ -2,28 +2,13 @@ package com.album2me.repost.domain.post.dto.response;
 
 import com.album2me.repost.domain.post.model.Post;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import org.springframework.beans.BeanUtils;
-
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class PostResponse {
-
-    private Long id;
-    private Long writerId;
-    private Long albumId;
-    private String title;
-    private String contents;
-    private Long commentCount;
-    private boolean isFavorite;
+public record PostResponse (
+    Long id,
+    String title,
+    String contents
+) {
 
     public static PostResponse from(final Post post) {
-        final PostResponse postResponse = new PostResponse();
-        BeanUtils.copyProperties(post, postResponse);
-
-        return postResponse;
+        return new PostResponse(post.getId(), post.getTitle(), post.getContents());
     }
 }
