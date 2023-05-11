28d006db3b5bcbd888997a39c0cc2ea11777bd61
wonjunYou <youwjune@gmail.com>
2023-05-11 14:41:22 +0900

    feat : Comment 엔티티 작성

--- src/main/java/com/album2me/repost/domain/comment/domain/Comment.java
+++ src/main/java/com/album2me/repost/domain/comment/domain/Comment.java
@@ -1,8 +1,12 @@
 package com.album2me.repost.domain.comment.domain;
 
 import com.album2me.repost.domain.post.model.Post;
+import com.album2me.repost.domain.user.model.User;
+
 import jakarta.persistence.*;
+
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +20,26 @@ public class Comment {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Builder
+    public Comment (
+            final Long id,
+            final Post post,
+            final User user,
+            final String contents
+    ) {
+        this.id = id;
+        this.post = post;
+        this.user = user;
+        this.contents = contents;
+    }
 }
