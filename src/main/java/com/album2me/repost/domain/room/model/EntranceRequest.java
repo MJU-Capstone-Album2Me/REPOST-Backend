cbae9cdf847e6ee1b3951d2998a6505447f6ed56
stophwan <jjw981210@gmail.com>
2023-05-20 23:59:56 +0900

    feat: EntranceRequest Entity 생성

---
+++ src/main/java/com/album2me/repost/domain/room/model/EntranceRequest.java
@@ -0,0 +1,32 @@
+package com.album2me.repost.domain.room.model;
+
+import com.album2me.repost.domain.user.model.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class EntranceRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private User requester;
+
+    private LocalDateTime requestedAt;
+
+}
