c48e58af7e6f6165b1325081940426a5e21518be
stophwan <jjw981210@gmail.com>
2023-05-21 16:20:59 +0900

    refactor: 아이디 길이 20자로 수정

--- src/main/java/com/album2me/repost/global/util/RegexUtils.java
+++ src/main/java/com/album2me/repost/global/util/RegexUtils.java
@@ -1,7 +1,7 @@
 package com.album2me.repost.global.util;
 
 public class RegexUtils {
-    public static final String AUTHID_REGEX = "^[가-힣0-9a-zA-Z]{4,10}$";
+    public static final String AUTHID_REGEX = "^[가-힣0-9a-zA-Z]{4,20}$";
     public static final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,20}$";
     public static final String NICKNAME_REGEX = "^[가-힣0-9a-zA-Z]{2,10}$";
 
