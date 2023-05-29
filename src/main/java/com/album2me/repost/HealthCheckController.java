5bc35d85d30cb3a2d86747bf82ff11c0c54881e3
stophwan <jjw981210@gmail.com>
2023-05-30 05:02:08 +0900

    check deploy

--- src/main/java/com/album2me/repost/HealthCheckController.java
+++ src/main/java/com/album2me/repost/HealthCheckController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/health-check")
 public class HealthCheckController {
+    
     @GetMapping
     public Long heathCheck() {
         return System.currentTimeMillis();
