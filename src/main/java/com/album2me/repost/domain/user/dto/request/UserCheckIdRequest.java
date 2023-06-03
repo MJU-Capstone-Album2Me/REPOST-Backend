6ec6c0881a5ac819592170492f091eb7e7536b1d
stophwan <jjw981210@gmail.com>
2023-06-04 03:26:04 +0900

    feat: 중복 체크 기능 검증 추가

--- src/main/java/com/album2me/repost/domain/user/dto/request/UserCheckIdRequest.java
+++ src/main/java/com/album2me/repost/domain/user/dto/request/UserCheckIdRequest.java
@@ -1,10 +1,13 @@
 package com.album2me.repost.domain.user.dto.request;
 
 import com.album2me.repost.global.util.RegexUtils;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record UserCheckIdRequest(
+        @NotNull
         @Pattern(regexp = RegexUtils.AUTHID_REGEX, message = "아이디는 한글, 숫자, 영어로 된 4~20자입니다.")
-        String userId
+        String authId
 ) {
 }
--- src/main/java/com/album2me/repost/domain/user/dto/request/UserCheckNicknameRequest.java
+++ src/main/java/com/album2me/repost/domain/user/dto/request/UserCheckNicknameRequest.java
@@ -1,6 +1,12 @@
 package com.album2me.repost.domain.user.dto.request;
 
+import com.album2me.repost.global.util.RegexUtils;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record UserCheckNicknameRequest(
+        @NotNull
+        @Pattern(regexp = RegexUtils.NICKNAME_REGEX, message = "닉네임은 한글, 숫자, 영어로 된 2~10자입니다.")
         String nickname
 ) {
 }
