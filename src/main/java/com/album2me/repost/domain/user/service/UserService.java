e32933b13209275da7d7f8529552b188a2642020
stophwan <jjw981210@gmail.com>
2023-06-04 03:20:15 +0900

    feat: 아이디 및 닉네임 중복 확인 기능 구현

--- src/main/java/com/album2me/repost/domain/user/service/UserService.java
+++ src/main/java/com/album2me/repost/domain/user/service/UserService.java
@@ -1,5 +1,8 @@
 package com.album2me.repost.domain.user.service;
 
+import com.album2me.repost.domain.user.dto.request.UserCheckIdRequest;
+import com.album2me.repost.domain.user.dto.request.UserCheckNicknameRequest;
+import com.album2me.repost.domain.user.dto.response.UserCheckResponse;
 import com.album2me.repost.domain.user.model.User;
 import com.album2me.repost.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +13,11 @@ import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
+
+    @Transactional
     public void signUp(User user){
         userRepository.save(user);
     }
@@ -25,5 +31,21 @@ public class UserService {
     public User findUserById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new NoSuchElementException("해당 id로 User를 찾을 수 없습니다."));
+    }
+
+    public UserCheckResponse checkIdDuplicated(UserCheckIdRequest userCheckIdRequest) {
+        String authId = userCheckIdRequest.userId();
+        if(userRepository.existsByAuthId(authId)){
+            return new UserCheckResponse(false);
+        }
+        return new UserCheckResponse(true);
+    }
+
+    public UserCheckResponse checkNicknameDuplicated(UserCheckNicknameRequest userCheckNicknameRequest) {
+        String nickname = userCheckNicknameRequest.nickname();
+        if(userRepository.existsByNickName(nickname)){
+            return new UserCheckResponse(false);
+        }
+        return new UserCheckResponse(true);
     }
 }
