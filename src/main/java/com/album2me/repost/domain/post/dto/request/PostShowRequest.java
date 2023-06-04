8188bfa5984fafba9590007178ce86398e45f673
wonjunYou <youwjune@gmail.com>
2023-06-05 08:28:31 +0900

    fix : 포스트 전체 조회 오류 해결

--- src/main/java/com/album2me/repost/domain/post/dto/request/PostShowRequest.java
+++ src/main/java/com/album2me/repost/domain/post/dto/request/PostShowRequest.java
@@ -3,7 +3,6 @@ package com.album2me.repost.domain.post.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record PostShowRequest(
-        @NotNull(message = "cursor가 없습니다.")
         Long cursor
 ) {
 
