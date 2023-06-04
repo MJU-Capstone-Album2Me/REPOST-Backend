ee04b7a024ee9578a3187dc9305fd87ff3b23dd8
stophwan <jjw981210@gmail.com>
2023-06-05 05:52:11 +0900

    feat: 프로필 사진 등록 기능 구현

--- src/main/java/com/album2me/repost/domain/user/controller/UserController.java
+++ src/main/java/com/album2me/repost/domain/user/controller/UserController.java
@@ -3,13 +3,19 @@ package com.album2me.repost.domain.user.controller;
 import com.album2me.repost.domain.user.dto.request.UserCheckIdRequest;
 import com.album2me.repost.domain.user.dto.request.UserCheckNicknameRequest;
 import com.album2me.repost.domain.user.dto.request.UserCreateRequest;
+import com.album2me.repost.domain.user.dto.request.UserProfileChangeRequest;
 import com.album2me.repost.domain.user.dto.response.UserCheckResponse;
+import com.album2me.repost.domain.user.dto.response.UserProfileChangeResponse;
 import com.album2me.repost.domain.user.service.UserService;
+import com.album2me.repost.global.config.security.jwt.JwtAuthentication;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +34,12 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public void signUp(@RequestBody @Valid UserCreateRequest userCreateRequest){
         userService.signUp(userCreateRequest.toEntity(passwordEncoder));
+    }
+
+    @PatchMapping("/profile-image")
+    public ResponseEntity<UserProfileChangeResponse> changeProfileImage(@ModelAttribute @Valid UserProfileChangeRequest userProfileChangeRequest,
+                                                                        @AuthenticationPrincipal JwtAuthentication jwtAuthentication) {
+        return ResponseEntity.ok(userService.changeProfileImage(userProfileChangeRequest, jwtAuthentication.getId()));
     }
 
     @PostMapping("/check-id")
