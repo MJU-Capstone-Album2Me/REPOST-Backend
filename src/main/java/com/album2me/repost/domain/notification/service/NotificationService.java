3048e12b526ad34dae2f0edf3b33791e61aaa46c
stophwan <jjw981210@gmail.com>
2023-06-05 07:25:45 +0900

    feat: 알림 조회 기능 구현

--- src/main/java/com/album2me/repost/domain/notification/service/NotificationService.java
+++ src/main/java/com/album2me/repost/domain/notification/service/NotificationService.java
@@ -1,10 +1,13 @@
 package com.album2me.repost.domain.notification.service;
 
+import com.album2me.repost.domain.notification.dto.NotificationListResponse;
+import com.album2me.repost.domain.notification.dto.NotificationResponse;
 import com.album2me.repost.domain.notification.model.Notification;
 import com.album2me.repost.domain.notification.model.NotificationType;
 import com.album2me.repost.domain.notification.repository.NotificationRepository;
 import com.album2me.repost.domain.room.model.RoomApply;
 import com.album2me.repost.domain.user.model.User;
+import com.album2me.repost.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +17,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class NotificationService {
     private final NotificationRepository notificationRepository;
+    private final UserService userService;
+
+    public NotificationListResponse getNotifications(Long loginId) {
+        User loginUser = userService.findUserById(loginId);
+        return new NotificationListResponse(notificationRepository.findAllByUserOrderByCreatedAtDesc(loginUser).stream()
+                .map(NotificationResponse::from).toList());
+    }
 
     @Transactional
     public void createApplyNotification(User user, User requester, RoomApply roomApply) {
