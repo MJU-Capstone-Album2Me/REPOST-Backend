a0cc6b1cc9f4d227fa6d66ca537ad9069267aa6a
wonjunYou <youwjune@gmail.com>
2023-06-06 23:40:24 +0900

    fix : 커서 방식 페이지네이션 입력 queryString으로 변경

--- src/main/java/com/album2me/repost/domain/post/dto/request/GalleryShowRequest.java
+++
@@ -1,9 +0,0 @@
-package com.album2me.repost.domain.post.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-
-public record GalleryShowRequest (
-        @NotNull(message = "cursor가 없습니다.")
-        Long cursor
-) {
-}
