ac74ff1faf861efaa3bff6d471a1224bc1ece397
stophwan <jjw981210@gmail.com>
2023-06-11 18:34:40 +0900

    feat: 유저 프로필 이미지 조회 기능 추가

--- src/main/java/com/album2me/repost/domain/user/controller/UserController.java
+++ src/main/java/com/album2me/repost/domain/user/controller/UserController.java
@@ -6,6 +6,7 @@ import com.album2me.repost.domain.user.dto.request.UserCreateRequest;
 import com.album2me.repost.domain.user.dto.request.UserProfileChangeRequest;
 import com.album2me.repost.domain.user.dto.response.UserCheckResponse;
 import com.album2me.repost.domain.user.dto.response.UserProfileChangeResponse;
+import com.album2me.repost.domain.user.dto.response.UserProfileImageResponse;
 import com.album2me.repost.domain.user.service.UserService;
 import com.album2me.repost.global.config.security.jwt.JwtAuthentication;
 import jakarta.validation.Valid;
@@ -14,13 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -34,6 +29,11 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public void signUp(@RequestBody @Valid UserCreateRequest userCreateRequest){
         userService.signUp(userCreateRequest.toEntity(passwordEncoder));
+    }
+
+    @GetMapping("/profile-image")
+    public ResponseEntity<UserProfileImageResponse> getProfileImage(@AuthenticationPrincipal JwtAuthentication jwtAuthentication) {
+        return ResponseEntity.ok(userService.getProfileImage(jwtAuthentication.getId()));
     }
 
     @PatchMapping("/profile-image")
